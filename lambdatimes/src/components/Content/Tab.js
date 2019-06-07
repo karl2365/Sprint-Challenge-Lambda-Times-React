443e58e5a5ce37c637c399bdb6a7ec2747f91884
Karl Beksa <karlbeksa@gmail.com>
2019-06-07 13:42:59 -0400

    MVP Complete

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -2,12 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Tab = props => {
+  console.log(props);
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+  
+  const isSelected = () => {
+    let tabClass='';
+    if (props.tab === props.selectedTab){
+      tabClass='tab active-tab';
+    }else{
+      tabClass='tab';
+    }
+    return tabClass;
+  }
+
   return (
     <div
-      className={''}
+      className={isSelected()}
       onClick={() => {props.selectTabHandler(props.tab)
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
